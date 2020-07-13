ed720b2eb2cfb9c1aa8a41f8c128890685ffb482
NikitaM33 <38262206+NikitaM33@users.noreply.github.com>
2020-07-13 15:52:56 +0300

    Add comments

--- index.js
+++ index.js
@@ -11,24 +11,29 @@ let name = '';
 let email = '';
 let phone = '';
 
+// Добавляю слушатель на input-ы 
 inputs.forEach((item) => {
     item.addEventListener('blur', function() {
         let rule = this.dataset.rule;
         let value = this.value;
         let check = false;
         
+
         switch(rule) {
 
-            // Name field check
+            // Проверка поля name
             case 'name':
                 let correctStr = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u.test(value);
                 
                 if(correctStr) {
+
+                    // Удаляю лишние пробелы и делаю массив из строки
                     let trimValue = value.trim();
                 
                     let nameArr = trimValue.split(' ');
                     this.value = '';
     
+                    // Проверяю каждую строку и заменяю первую букву каждого слова на заглавную
                     nameArr.forEach((arrItem) => {
                         if(value) {
                             let uperLetter = arrItem[0].toUpperCase();
@@ -39,32 +44,40 @@ inputs.forEach((item) => {
                             name = this.value;
                         }
                         
+                        // Если стоят инициалы
                         if(arrItem.length <= 2) {
                             nameError.innerHTML = 'ФИО должны быть полными!';
                             check = false;
                         }
                     })
                 } else {
+                    // Если поле пустое
                     nameError.innerHTML = 'Поле должно содержать буквы!';
                     check = false;
                 }
 
             break;
 
-            // Email check
+            // Проверка поля Email
             case 'email':
+
+                // Удаляю лишние пробелы и проверяю заканчивается ли почтовый адрес на @gmail.com
                 let trimEmail = value.trim();
                 let emailCheck = /@gmail.com$/g.test(trimEmail);
                 
-
+                // Если все корректно
                 if(emailCheck) {
                     check = true;
                     emailError.innerHTML = '';
                     email = this.value;
                 } else {
+
+                    // Если поле пустое
                     if(this.value === '') {
                         emailError.innerHTML = 'Поле обязательно для заполнения!';
                     } else {
+
+                        // Если почтовый адрес приндлежит другому домену
                         check = false;
                         emailError.innerHTML = 'Email должен принадлежать домену gmail.com!';
                     }
@@ -72,14 +85,17 @@ inputs.forEach((item) => {
 
             break;
 
-            // Phone check
+            // Проверка телефона
             case 'phone':
+
+                // Удаляю лишние пробелы и убераю все не цифровые символы
                 let trimPhone = value.trim();
                 let phoneCheck = trimPhone.replace(/\D/g, "");
 
+                // Телефон должен быть не меньше 7 и не больше 11 символов
                 if(phoneCheck.length < 7 || phoneCheck.length > 11) {
                     check - false;
-                    phoneError.innerHTML = 'Телефон должен быть не менее 7 цифр и не более 11!';
+                    phoneError.innerHTML = 'Телефон должен быть менее 7 цифр и не более 11!';
                 } else {
                     check = true;
                     phoneError.innerHTML = '';
@@ -89,6 +105,7 @@ inputs.forEach((item) => {
             break;
         }
 
+        // Удаляю классы valid/invalid перед тем как вставить новый
         this.classList.remove('invalid');
         this.classList.remove('valid');
 
@@ -101,6 +118,8 @@ inputs.forEach((item) => {
     })
 })
 
+
+// Слушатель на кнопке "Отправить"
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
     let isSend = false;
@@ -111,6 +130,7 @@ submitBtn.addEventListener('click', (event) => {
         phone
     }
 
+    // Проверка есть ли в полях значения
     for(key in data) {
         if(!data[key]) {
             noField.classList.remove('successSend');
@@ -122,17 +142,21 @@ submitBtn.addEventListener('click', (event) => {
         }
     }
 
+
+    // Если все заполнено отправляю запрос
     if(isSend) {
         sendForm('POST', testUrl, data)
         .then(data => console.log(data))
         .catch(err => console.log(err));
 
+        // Оповещаю, что данные были отправлены успешно
         noField.classList.remove('required');
         noField.classList.add('successSend');
         noField.innerHTML = 'Форма отправлена';
     }
 })
 
+// Функция отправки данных
 async function sendForm(method, url, data = null) {
     const headers = {
         'Content-Type': 'application/json'
